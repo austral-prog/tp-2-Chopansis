61ad218cfe00ac34e1c8fa43b9be88167aa07c63
Francisco Garcia Nirenstein <francisgarcianirenstein@gmail.com>
2025-04-03 15:40:00 -0300

    Add files via upload

--- change.py
+++ change.py
@@ -1,3 +1,17 @@
 def change():
     expense = 23.75
     money = 100
+    vuelto_entero = (money - expense) //1
+    vuelto_decimal = (money - expense) - vuelto_entero
+    print("Ingresar gasto:")
+    print (f"{expense}")
+    print("Dinero recibido")
+    print(money)
+    print(" ")
+    print ("Vuelto")
+    print(" ")
+    print("Pesos:")
+    print (f"{vuelto_entero}")
+    print("Centavos:")
+    print(f"{vuelto_decimal}")
+change()
